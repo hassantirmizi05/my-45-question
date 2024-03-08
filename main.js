ffeccd28603f97b6601775105c69977db611e2d3
hassantirmizi05 <160477628+hassantirmizi05@users.noreply.github.com>
2024-03-08 05:26:37 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 //friends name in an array
 var friendsname = ["ali", "maaz", "aryan", "hassan"];
-friendsname.forEach(function (friendname) { return console.log(friendname); });
+friendsname.forEach(function (friendname) { return console.log("assalamualaikum ".concat(friendname, ", may allah bless you ameen. ")); });
--- main.ts
+++ main.ts
@@ -2,4 +2,5 @@
 
 let friendsname = ["ali","maaz","aryan","hassan"];
 
-friendsname.forEach(friendname => console.log(friendname));
+friendsname.forEach(friendname => console.log(`assalamualaikum ${friendname}, may allah bless you ameen. `));
+
