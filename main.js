1cc6400d61cd0906f4517bcd54758c74a6975b45
hassantirmizi05 <160477628+hassantirmizi05@users.noreply.github.com>
2024-03-08 05:28:35 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
+// creating guest list array
 var allguests = ["ali", "maaz", "aryan"];
+// creating variable for unavailable guests
 var notComing = allguests[2];
+// print name of unavailable guest
 console.log(notComing, "is busy somwhere");
+// adding and removing specific guest
 allguests.splice(2, 2, "faizan");
-allguests.forEach(function (guest) { return console.log("hello ".concat(guest, " would you like to dinner with me?")); });
+// message for bigger table
+console.log("good news! , we have found a bigger dinner table");
+// add new guest at starting index of array
+allguests.unshift("aashir");
+// add new guest at end index of array
+allguests.push("rehan");
+// get middle index of array by average(middle value)
+var middleindex = Math.floor(allguests.length / 2);
+// add new guest at middle of index
+allguests.splice(middleindex, 0, "hamza");
+// print updated list
+console.log("updated list of our guests");
+// print particular guests with message
+allguests.forEach(function (oneguest) { return console.log("hello ".concat(oneguest, ", would you like to dinner with me?")); });
--- main.ts
+++ main.ts
@@ -1,6 +1,42 @@
-let allguests = ["ali","maaz","aryan"];
-let notComing = allguests[2];
-console.log(notComing,"is busy somwhere");
-allguests.splice(2,2,"faizan");
-allguests.forEach(guest => console.log(`hello ${guest} would you like to dinner with me?`));
+// creating guest list array
 
+
+let allguests = ["ali","maaz","aryan"];
+
+// creating variable for unavailable guests
+
+let notComing = allguests[2];
+
+// print name of unavailable guest
+
+console.log(notComing,"is busy somwhere");
+
+// adding and removing specific guest
+
+allguests.splice(2,2,"faizan");
+
+// message for bigger table
+
+console.log("good news! , we have found a bigger dinner table");
+
+// add new guest at starting index of array
+
+allguests.unshift("aashir");
+
+// add new guest at end index of array
+
+allguests.push("rehan");
+
+// get middle index of array by average(middle value)
+
+let middleindex : number = Math.floor(allguests.length / 2);
+
+// add new guest at middle of index
+
+allguests.splice(middleindex, 0 , "hamza");
+
+// print updated list
+console.log("updated list of our guests");
+
+// print particular guests with message
+allguests.forEach(oneguest => console.log(`hello ${oneguest}, would you like to dinner with me?`));
