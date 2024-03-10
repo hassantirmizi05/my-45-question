4aabbf626e8ecf0091b25edeb0c0b0e90e8b7f89
hassantirmizi05 <160477628+hassantirmizi05@users.noreply.github.com>
2024-03-11 02:53:59 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,6 @@
-// Seeing the World, array of places original order
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
-        }
-    }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
-var places = ["haram", "madinah", "turkey", "iraq", "palestine"];
-console.log("original order is ".concat(places));
-// copy of array in alphabetical order without modifying
-console.log("alphabtical order", __spreadArray([], places, true).sort());
-// showing the original order
-console.log("original order ".concat(places));
-// copy of array in reverse order
-console.log("reverse order", __spreadArray([], places, true).reverse());
-// showing the original order
-console.log("original order ".concat(places));
-// changed the order of original array without copying 
-console.log("original array reversed", places.reverse());
-// back to original order 
-console.log("back to original", places.reverse());
-// sorting original array again in alphabetical order
-console.log("alphabetical order", places.sort());
-// reversing the original array
-console.log("reversed original array again", places.reverse());
+//inviting guests for dinner
+var guests = ["ali", "maaz", "aryan", "hassan"];
+// let invitation = "i would like to invite you for dinner at my home";
+// guests.forEach(person => console.log(person,invitation));
+var lengthguest = guests.length;
+console.log("we are inviting total ".concat(lengthguest, " guests"));
--- main.ts
+++ main.ts
@@ -1,28 +1,8 @@
-// Seeing the World, array of places original order
+//inviting guests for dinner
+let guests = ["ali","maaz","aryan","hassan"];
+// let invitation = "i would like to invite you for dinner at my home";
+// guests.forEach(person => console.log(person,invitation));
+let lengthguest : number = guests.length;
 
-let places:string[] = ["haram","madinah","turkey","iraq","palestine"];
-console.log(`original order is ${places}`);
+console.log(`we are inviting total ${lengthguest} guests`);
 
-// copy of array in alphabetical order without modifying
-console.log(`alphabtical order`,[...places].sort());
-
-// showing the original order
-console.log(`original order ${places}`);
-
-// copy of array in reverse order
-console.log("reverse order", [...places].reverse());
-
-// showing the original order
-console.log(`original order ${places}`);
-
-// changed the order of original array without copying 
-console.log("original array reversed",places.reverse());
-
-// back to original order 
-console.log("back to original",places.reverse());
-
-// sorting original array again in alphabetical order
-console.log("alphabetical order",places.sort());
-
-// reversing the original array
-console.log("reversed original array again",places.reverse());
